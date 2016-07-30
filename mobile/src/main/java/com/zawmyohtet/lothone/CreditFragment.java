a7de769f1ed6e3c4db7cc278016bb03934a62236
Zaw Myo Htet <hackforpeace01@gmail.com>
2016-07-30 16:46:11 +0630

    add personal info edit view

--- mobile/src/main/java/com/zawmyohtet/lothone/CreditFragment.java
+++ mobile/src/main/java/com/zawmyohtet/lothone/CreditFragment.java
@@ -12,8 +12,6 @@ public class CreditFragment extends Fragment {
 
     public static final String TAG = "CreditFragment";
 
-    private OnFragmentInteractionListener mListener;
-
     public CreditFragment() {
         // Required empty public constructor
     }
@@ -31,40 +29,16 @@ public class CreditFragment extends Fragment {
     }
 
     public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
--- mobile/src/main/java/com/zawmyohtet/lothone/HomeFragment.java
+++ mobile/src/main/java/com/zawmyohtet/lothone/HomeFragment.java
@@ -2,8 +2,11 @@ package com.zawmyohtet.lothone;
 
 import android.app.Fragment;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,7 +19,7 @@ public class HomeFragment extends Fragment {
 
     public static final String TAG = "HomeFragment";
 
-    private OnFragmentInteractionListener mListener;
+    private Context context;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -33,15 +36,18 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         ButterKnife.bind(this, view);
+        context = getActivity();
         return view;
     }
 
     @OnClick({R.id.cv_police, R.id.cv_fire_brigade, R.id.cv_ambulance, R.id.cv_ministry, R.id.cv_area, R.id.cv_zip})
-    public void handleClick(View v){
-        switch (v.getId()){
+    public void handleClick(View v) {
+        switch (v.getId()) {
             case R.id.cv_police:
+                context.startActivity(new Intent(context, PersonalInfoActivity.class));
                 break;
             case R.id.cv_fire_brigade:
+                Log.d(TAG, "It is on click.");
                 break;
             case R.id.cv_ambulance:
                 break;
@@ -54,42 +60,16 @@ public class HomeFragment extends Fragment {
         }
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
     public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
